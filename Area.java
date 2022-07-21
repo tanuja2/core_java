65523c9f5ce4a06a463665384e06bd6d5c48bb13
tanuja2 <109609781+tanuja2@users.noreply.github.com>
2022-07-21 12:36:26 -0230

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,16 @@
+class Area
+{
+	public static void main(String args[])
+	{
+		double square,rectangle,side,length,width;
+		side=3;
+		length=4;
+		width=4;
+		square=side*side;
+		System.out.println("area of sqaure:"+square);
+		rectangle=length*width;
+		System.out.println("area of rectangle:"+rectangle);
+	}
+}
+
+	
