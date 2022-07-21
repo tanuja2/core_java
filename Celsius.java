eac688bff604ff0195308871fd63766d71a1920b
tanuja2 <109609781+tanuja2@users.noreply.github.com>
2022-07-21 12:45:21 -0230

    Add files via upload

---
+++ Celsius.java
@@ -0,0 +1,10 @@
+class Celsius
+{
+public static void main(String args[])
+{
+float fahrenheit,celsius;
+fahrenheit=3.2f;
+celsius=((fahrenheit-32)*5/9);
+System.out.println("temperature in celsius:"+fahrenheit);
+}
+}
