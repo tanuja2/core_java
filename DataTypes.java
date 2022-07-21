964f12e39e5770a613819f2e741de9c162bb68f2
tanuja2 <109609781+tanuja2@users.noreply.github.com>
2022-07-21 13:07:02 -0230

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,28 @@
+class DataTypes 
+{
+public static void main(String args[])
+{
+int num1=300;
+short myshort=34;
+byte mybyte=126;
+long mylong=1000000000L;
+float myfloat=0.9f;
+double mydouble=6.77888;
+char mychar='a';
+boolean mybool= false;
+System.out.println("Integer no.is:"+num1);
+System.out.println("shortInteger no.is:"+myshort);
+System.out.println("byte Integeral no.is:"+mybyte);
+System.out.println("long Integer no.is:"+mylong);
+System.out.println("floating point no.is:"+myfloat);
+System.out.println("double floating point no.is:"+mydouble);
+System.out.println("My first character is:"+mychar);
+if(mybool=false);
+  {
+    System.out.println("my boolean data is:"+mybool);
+  }
+}
+}
+
+
+
