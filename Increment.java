a2f6305f07cacc888b30b0c8977c1961b45dcd49
tanuja2 <109609781+tanuja2@users.noreply.github.com>
2022-07-21 11:13:44 -0230

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,17 @@
+class Increment
+{
+	public static void main(String args[])
+	{
+		int num1,num2,sum,difference,average,product;
+		num1=20;
+		num2=21;
+		sum=num1+num2;
+		System.out.println("sum of two no.="+sum);
+		difference=num2-num1;
+		System.out.println("difference of two no.="+difference);	
+		average=(num1+num2)/2;
+     	System.out.println("average of two no.="+average);
+		product=num1*num2;
+	    System.out.println("product of two no.="+product);
+	}
+}
