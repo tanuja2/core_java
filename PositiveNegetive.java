5cfc0e13fd0d9ef1843c59c9cd1f4394f9ff9c05
tanuja2 <109609781+tanuja2@users.noreply.github.com>
2022-07-22 12:50:37 -0230

    Add files via upload

---
+++ PositiveNegetive.java
@@ -0,0 +1,17 @@
+//progrm to check the given number is positive or negetive.
+class PositiveNegetivenumber
+{
+public static void main(String args[])
+{
+int number= -13;
+if(number>0)
+{
+System.out.println("Positive");
+}
+else
+{
+System.out.println("Negetive");
+}
+}
+}
+
